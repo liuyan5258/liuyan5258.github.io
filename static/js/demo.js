4f66447248927d4ede9bf42f41bedb6edf057151
liu_yan <liu_yan@corp.netease.com>
2015-10-10 15:42:31 +0800

    add a svg demo

--- static/js/demo.js
+++ static/js/demo.js
@@ -18,29 +18,42 @@ $(function(){
           length, //定义path所生成的素描长度
           handle, //定义javascript动画句柄
           path = document.getElementsByTagName('path'),
-          length = path[0].getTotalLength();
-            
-    //定义初始化方法
-    var init = function(){
-      current_frame = 0;
-      total_frames = 160;
-      path[0].style.strokeDasharray = length + ' ' + length; //定义dasharray
-      path[0].style.strokeDashoffset = length; //定义dashoffset
-      handle = 0;
+          length;
+    
+    function main(n){
+      length = path[n].getTotalLength();
+
+      //定义初始化方法
+      var init = function(){
+        current_frame = 0;
+        total_frames = 160;
+        path[n].style.strokeDasharray = length + ' ' + length; //定义dasharray
+        path[n].style.strokeDashoffset = length; //定义dashoffset
+        handle = 0;
+      };
+      //定义实际的动画绘制方法
+      var draw = function(){
+        var progress = current_frame/total_frames;
+        if(progress>1){ //这里定义完成动画
+          window.cancelAnimationFrame(handle);
+        }else{//否则使用reqeuestAnimationFrame来生成动画
+          current_frame++;
+          path[n].style.strokeDashoffset = Math.floor(length*(1 - progress));
+          
+          handle = window.requestAnimationFrame(draw);
+        }
+      };
+      init();
+      draw();
+    }    
+    
+    for(var i = 0; i < path.length; i++) {
+      (function(e) {
+        setTimeout(function() {
+            main(e);
+        }, 2000);
+      })(i);
     }
-    //定义实际的动画绘制方法
-    var draw = function(){
-      var progress = current_frame/total_frames;
-      if(progress>1){ //这里定义完成动画
-        window.cancelAnimationFrame(handle);
-      }else{//否则使用reqeuestAnimationFrame来生成动画
-        current_frame++;
-        path[0].style.strokeDashoffset = Math.floor(length*(1 - progress));
-        
-        handle = window.requestAnimationFrame(draw);
-      }
-    }
-    init();
-    draw();
+    
   });
 });
